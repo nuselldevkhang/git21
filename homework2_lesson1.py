0d9961dbf3116743913ef6573eafab42638e2743
nuselldevkhang <dinhkhang6596@gmail.com>
2021-11-21 23:48:55 +0700

    add homework2_lesson1.py

---
+++ homework2_lesson1.py
@@ -0,0 +1,25 @@
+""" Các kiểu dữ liệu trong bài học:
++ Bool
++ Number: int, float, complex
++ Characters, string
++ sequence,set, ...
+"""
+#Bool
+if 1 != 2:
+    print("1 khac 2")
+
+#Number
+a = 1
+print(f"{a} la so nguyen ")
+b = 2.5
+print(f"{b} la so thuc")
+c = complex(1,2)
+print(f"{c} la so phuc")
+
+#character
+str1 = "Have a good day"
+print(str1)
+
+#sequence
+list1 = [1,2,3,"hoa hoa",'a1',"nn"]
+print(list1)
---
+++ homework3_lesson1.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+print(f"welcone to {name}")
+year_of_birth = int(input("what year you born?: "))
+year_now = 2021 - year_of_birth
+print(f"Now, you are {year_now} years old")
